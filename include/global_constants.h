1e612544984440cfcca868b85364517e8f938a62
Bamamou <bamamounicolas@gmail.com>
2023-03-12 17:49:05 +0800

    little changes

---
+++ include/global_constants.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * global_Constants.h
+ * Constants that will be used in the whole project
+ *****************************************************************************/
+#ifndef __GLOBAL_CONSTANTS_H__
+#define __GLOBAL_CONSTANTS_H__
+#include <Arduino.h>
+
+
+int delayTime =1000;   // Set the delay time between each values 
+int counter;          // data counter
+const int totalColumns = 16;
+const int totalRows    = 2;
+
+#endif
---
+++ include/main.h
@@ -0,0 +1,15 @@
+/******************************************************************************
+ * main.h
+ * Header for the main file
+ *****************************************************************************/
+#ifndef __MAIN_H__
+#define __MAIN_H__
+#include<Arduino.h>
+#include<global_constants.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include "DHTesp.h" // Click here to get the library: http://librarymanager/All#DHTesp
+
+
+
+#endif
